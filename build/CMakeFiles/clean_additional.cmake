7ba4a66f5aa0510713586f3cd3a34ae945b43cc8
antonioasaro <antonio_asaro@hotmail.com>
2023-03-18 21:14:28 -0400

    .gitignore sdkconfig

--- build/CMakeFiles/clean_additional.cmake
+++ build/CMakeFiles/clean_additional.cmake
@@ -1,7 +1,7 @@
 # Additional clean files
 cmake_minimum_required(VERSION 3.16)
 
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
   file(REMOVE_RECURSE
   "../components/micro_ros_espidf_component/esp32_toolchain.cmake"
   "../components/micro_ros_espidf_component/include"
