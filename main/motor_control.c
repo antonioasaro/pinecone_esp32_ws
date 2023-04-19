298a863c5c1904cd3e5f33873db460a1fc8ea094
antonioasaro <antonio_asaro@hotmail.com>
2023-04-19 19:34:00 -0400

    Settings for 2nd motor

--- main/motor_control.c
+++ main/motor_control.c
@@ -24,11 +24,11 @@ static const char *TAG = "motor_control";
 #define BDC_MCPWM_TIMER_RESOLUTION_HZ 10000000                                      // 10MHz, 1 tick = 0.1us
 #define BDC_MCPWM_FREQ_HZ 25000                                                     // 25KHz PWM
 #define BDC_MCPWM_DUTY_TICK_MAX (BDC_MCPWM_TIMER_RESOLUTION_HZ / BDC_MCPWM_FREQ_HZ) // maximum value we can set for the duty cycle, in ticks
-#define BDC_MCPWM_GPIO_A 18
-#define BDC_MCPWM_GPIO_B 19
+#define BDC_MCPWM_GPIO_A 27 // 18
+#define BDC_MCPWM_GPIO_B 26 // 19
 
-#define BDC_ENCODER_GPIO_A 16
-#define BDC_ENCODER_GPIO_B 17
+#define BDC_ENCODER_GPIO_A 14 // 16
+#define BDC_ENCODER_GPIO_B 12 // 17
 #define BDC_ENCODER_PCNT_HIGH_LIMIT 1000
 #define BDC_ENCODER_PCNT_LOW_LIMIT -1000
 
